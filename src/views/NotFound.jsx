8df4b243b9f03bc1e316b561cda391ca92d78432
LePetitLouis <44061639+LePetitLouis@users.noreply.github.com>
2021-09-21 18:54:39 +0200

    page notfound

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -3,21 +3,45 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 // Material components
-import { Box, Container, Typography } from '@material-ui/core';
+import { Box, CardMedia, Container, makeStyles, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    container: {
+        minHeight: '80vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    img: {
+        width: '60%',
+        margin: 'auto',
+        paddingBottom: '4%'
+    },
+    link: {
+        color: '#1875D1',
+        textDecoration: 'underline'
+    }
+}))
 
 function NotFound() {
+    const classes = useStyles()
 
     return (
-        <Container>
+        <Container className={classes.container}>
             <Box>
-                <Typography variant="h1" component="h2">
-                    Oups !
-                </Typography>
-                <Typography variant="h3" component="h2">
+                <CardMedia
+                    component="img"
+                    image="/assets/notfound.svg"
+                    title="erreur 404"
+                    className={classes.img}
+                />
+                <Typography variant="h3" component="h2" align='center'>
                     La page que vous recherchez semble introuvable.
                 </Typography>
 
-                <Link to="/">Page d'accueil</Link>
+                <Typography>
+                    <Link to="/" className={classes.link}>Revenir à la page d'accueil</Link>
+                </Typography>
             </Box>
         </Container>
     );
